6756114a9e3f6a0c664a7346ac7843448b9047a8
E-Mello <e.mello.dev@gmail.com>
2022-10-14 17:49:45 -0400

    Add Config Connection

---
+++ nodeOracleRestApi/checkConnection.js
@@ -0,0 +1,28 @@
+const oracledb = require('oracledb');
+// hr schema password
+var password = 'pereira2022' 
+// checkConnection asycn function
+async function checkConnection() {
+  try {
+    connection = await oracledb.getConnection({
+        user: "EDIO.PEREIRA",
+        password: password,
+        connectString: "192.168.1.130:1521/Solustst"
+    });
+    console.log('connected to database');
+  } catch (err) {
+    console.error(err.message);
+  } finally {
+    if (connection) {
+      try {
+        // Always close connections
+        await connection.close(); 
+        console.log('close connection success');
+      } catch (err) {
+        console.error(err.message);
+      }
+    }
+  }
+}
+
+checkConnection();
