d37a4d0aa8277f58e87582c7ab003aac3b4011c6
Edio Mello <e.mello.dev@gmail.com>
2022-10-23 21:06:10 -0400

    Add folder dbversions

---
+++ dbVersions/equipment.sql
@@ -0,0 +1,80 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Linux (x86_64)
+--
+-- Host: localhost    Database: equipment
+-- ------------------------------------------------------
+-- Server version	8.0.30-0ubuntu0.22.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `equipamento`
+--
+
+DROP TABLE IF EXISTS `equipamento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `equipamento` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `modelo` varchar(255) NOT NULL,
+  `serie` varchar(255) NOT NULL,
+  `id-prestador` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `equipamento`
+--
+
+LOCK TABLES `equipamento` WRITE;
+/*!40000 ALTER TABLE `equipamento` DISABLE KEYS */;
+INSERT INTO `equipamento` VALUES (1,'Biometrico','Digital001','00000M54',0),(2,'Biometrico','Digital001','1111TG4',0),(3,'Cartao','Cartao001','555HTTX',0),(4,'Cartao','Cartao001','888JH32',0);
+/*!40000 ALTER TABLE `equipamento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `prestador`
+--
+
+DROP TABLE IF EXISTS `prestador`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `prestador` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `cpf` varchar(255) NOT NULL,
+  `id-equipamento` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `prestador`
+--
+
+LOCK TABLES `prestador` WRITE;
+/*!40000 ALTER TABLE `prestador` DISABLE KEYS */;
+INSERT INTO `prestador` VALUES (1,'FUNDACAO DE SAUDE COMUNITARIA DE SINOP','02441244183',NULL),(2,'HOSPITAL E MATERNIDADE DOIS PINHEIROS','25412396314',NULL),(3,'HOSPITAL E MATERNIDADE 13 DE MAIO','78521456321',NULL),(4,'RAFAEL CASTILLO DA SILVA','52846132589',NULL);
+/*!40000 ALTER TABLE `prestador` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-23 21:03:37
---
+++ nodeOracleRestApi/API/index.js
@@ -0,0 +1,99 @@
+const express = require('express')
+const oracledb = require('oracledb');
+// import express from 'express'
+// import oracledb from 'oracledb'
+const app = express();
+const port = 3030;
+const password = 'pereira2022';
+
+async function selectAllEmployees(req, res) {
+    try {
+        connection = await oracledb.getConnection({
+            user: "EDIO.PEREIRA",
+            password: password,
+            connectString: "192.168.1.130:1521/Solustst"
+        });
+
+        console.log('connected to database');
+        // run query to get employees
+        result = await connection.execute(`SELECT NNUMEPESS,CNOMEPESS FROM SOLUS.HSSPESS 
+        WHERE NNUMEPESS BETWEEN 197911 AND 200000`);
+
+    } catch (err) {
+        //send error message
+        return res.send(err.message);
+    } finally {
+        if (connection) {
+            try {
+                // Always close connections
+                await connection.close();
+                console.log('close connection success');
+            } catch (err) {
+                console.error(err.message);
+            }
+        }
+        if (result.rows.length == 0) {
+            //query return zero employees
+            return res.send('query send no rows');
+        } else {
+            //send all employees
+            return res.send(result.rows);
+        }
+
+    }
+}
+
+//get /employess
+app.get('/employees', function (req, res) {
+    selectAllEmployees(req, res);
+})
+
+// async function selectEmployeesById(req, res, id) {
+//     try {
+//         connection = await oracledb.getConnection({
+//             user: "EDIO.PEREIRA",
+//             password: password,
+//             connectString: "192.168.1.130:1521/Solustst"
+//         });
+//         // run query to get employee with employee_id
+//         result = await connection.execute(`SELECT * FROM SOLUS.HSSPESS 
+//         WHERE NNUMEPESS BETWEEN 197911 AND 200000`, [id]);
+
+//     } catch (err) {
+//         //send error message
+//         return res.send(err.message);
+//     } finally {
+//         if (connection) {
+//             try {
+//                 // Always close connections
+//                 await connection.close();
+//             } catch (err) {
+//                 return console.error(err.message);
+//             }
+//         }
+//         if (result.rows.length == 0) {
+//             //query return zero employees
+//             return res.send('query send no rows');
+//         } else {
+//             //send all employees
+//             return res.send(result.rows);
+//         }
+//     }
+// }
+
+//get /employee?id=<id employee>
+app.get('/employee', function (req, res) {
+    //get query param ?id
+    let id = req.query.id;
+    // id param if it is number
+    if (isNaN(id)) {
+        res.send('Query param id is not number')
+        return
+    }
+    selectEmployeesById(req, res, id);
+})
+
+// console.log(result);
+
+
+app.listen(port, () => console.log("IndexJS app listening on port %s!", port))
