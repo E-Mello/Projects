7f834cbd0ddb52583d21cc41fe398e711a72925a
E-Mello <e.mello.dev@gmail.com>
2023-01-10 17:47:49 -0400

    Add Adjusts

---
+++ typescript/1673386906535-CreateCategories.js
@@ -0,0 +1,11 @@
+const { MigrationInterface, QueryRunner } = require("typeorm");
+
+module.exports = class CreateCategories1673386906535 {
+
+    async up(queryRunner) {
+    }
+
+    async down(queryRunner) {
+    }
+
+}
---
+++ typescript/1673386935732-CreateCategories.js
@@ -0,0 +1,11 @@
+const { MigrationInterface, QueryRunner } = require("typeorm");
+
+module.exports = class CreateCategories1673386935732 {
+
+    async up(queryRunner) {
+    }
+
+    async down(queryRunner) {
+    }
+
+}
